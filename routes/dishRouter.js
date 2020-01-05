d3c60f6da35c5b3fba4fbb06c64d8fdc4bf138bf
Aaron Carnes <acarnes@Trinity.localdomain>
2020-01-05 07:22:13 -0800

    Exercise 1 complete

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -22,23 +22,24 @@ dishRouter.route('/').all((req, res, next) => {
 .delete((req, res, next) => {
     res.end('Deleting all the dishes!');
 });
-//
-//dishRouter.route('/:dishId').all((req,res,next) => {
-//    res.statusCode = 200;
-//    res.setHeader('Content-Type', 'text/plain');
-//    next();
-//}).get( (req, res, next) => {
-//    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
-//}) 
-//.post( (req, res, next) => {
-//    res.end('POST operation not supported on /dishes/' + req.params.dishId );
-//}) 
-//put( (req, res, next) => {
-//    res.write('Updating the dish: ' + req.params.dishId);
-//    res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
-//})
-//delete( (req, res, next) => {
-//    res.end('Deleting dish: ' + req.params.dishId);
-//});
-//
-module.exports = dishRouter;
+
+dishRouter.route('/:dishId').all((req, res, next) =>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get( (req, res, next) => {
+    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+})
+.post( (req, res, next) => {
+    res.end('POST operation not supported on /dishes/' + req.params.dishId );
+}) 
+.put( (req, res, next) => {
+    res.write('Updating the dish: ' + req.params.dishId + ' ');
+    res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
+}) 
+.delete( (req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+});
+
+module.exports= dishRouter;
